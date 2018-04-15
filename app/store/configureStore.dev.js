59cba9211dbe1113962a36576acaf43a76341967
Greg Huels <greg.huels@gmail.com>
2018-04-15 10:18:35 -0500

    Initial commit

---
+++ app/store/configureStore.dev.js
@@ -0,0 +1,19 @@
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import thunk        from 'redux-thunk';
+
+import StockApp     from '../reducers';
+
+const createDevStoreWithMiddleware = composeWithDevTools({})(
+  applyMiddleware(thunk),
+)(createStore);
+
+export default function configureStore() {
+  const store = createDevStoreWithMiddleware(StockApp);
+
+  if (module.hot) {
+    module.hot.accept(() => store.replaceReducer(require('../reducers')));
+  }
+
+  return store;
+}
---
+++ app/store/configureStore.js
@@ -0,0 +1,5 @@
+const requireDev = () => require('./configureStore.dev').default;
+
+const requireProd = () => require('./configureStore.prod').default;
+
+module.exports = process.env.NODE_ENV === 'development' ? requireDev() : requireProd();
